d1576cf30fd4cdbee32c4eae35a2e3ec8e7ef293
Salim Oussayfi <box@slimou.net>
2017-02-19 18:40:01 +0100

    new progs

--- 004_weitereGrundlagen/007_if-Abfragen.php
+++ 004_weitereGrundlagen/007_if-Abfragen.php
@@ -5,5 +5,33 @@
  * Date: 17.02.17
  * Time: 16:21
  */
-echo "if-Abfragen" . " noch ok!";
+
+$name = false;
+$zahl = 13;
+$nachname = "Castorp";
+
+if($name){
+    echo "Die Variable ist true";
+}else{
+    echo "Die Variable ist false";
+}
+echo "<br/>";
+
+if($zahl > 42){
+    echo "Teil 1";
+} elseif($zahl > 21) {
+    echo "Teil 2";
+} else {
+    echo "Teil 3";
+}
+
+
+echo "<br/>";
+
+if(!empty($nachname)){
+    echo "Name gesetzt";
+}else {
+    echo "Name nicht gesetzt";
+}
+
 ?>
